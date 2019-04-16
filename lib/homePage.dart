d05b41c129816d3af5214835fab4ff4d401c18f1
wtg <815786482@qq.com.com>
2019-04-16 17:01:44 +0800

    1.首页布局

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
 
 class HomePage extends StatefulWidget{
 
@@ -7,14 +8,34 @@ class HomePage extends StatefulWidget{
 }
 
 
-
 ///微信首页
 class _HomePageState extends State<HomePage>{
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("首页",style: TextStyle(color: Colors.greenAccent,fontSize: 30.0),),
+      child: FutureBuilder(
+          future: DefaultAssetBundle.of(context).loadString("json/chat_json.json"),
+          builder: (context,snapshot){
+            List mData = json.decode(snapshot.data.toString());
+            return ListView.builder(itemCount: mData.length,itemBuilder: (BuildContext context,int index){
+              print(index);
+              return new Padding(
+                padding: EdgeInsets.only(left: 10.0,top: 10.0,right: 10.0,bottom: 0.0),
+                child: new Row(
+                  children: <Widget>[
+                    Image.network(mData[index]["image"],width: 50.0,height: 50.0,fit: BoxFit.none,),
+                    Column(
+                      children: <Widget>[
+                        Stack(),
+
+                      ],
+                    )
+                  ],
+                ),
+              );
+            });
+          }),
     );
   }
 }
